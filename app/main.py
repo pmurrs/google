9af1418adae3e9b06d4aa25e5fec3ec175c7387b
CORP\paul.murray <paul.murray@cantire.com>
2018-06-22 02:07:49 -0400

    new commit

--- app/main.py
+++ app/main.py
@@ -91,7 +91,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            #return redirect("/image/" + filename, code=302)
+            return redirect("/image/" + filename, code=302)
     return '''
     <!doctype html>
     <title>Upload new File</title>
